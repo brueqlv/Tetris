c96b8addd944dfd0084827c5501c8eea33d98250
brueqlv <gundarens@gmail.com>
2023-12-14 09:18:51 +0200

    Add project files.

---
+++ Tetris/GameGrid.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    public class GameGrid
+    {
+        private readonly int[,] grid;
+        public int Rows { get; }
+        public int Columns { get; }
+
+        public int this[int r, int c]
+        {
+            get => grid[r, c];
+            set => grid[r, c] = value;
+        }
+
+    }
+}
